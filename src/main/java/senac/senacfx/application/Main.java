d91f1bed2eb9d4a713f6a0877a38ef68f32d489e
JohnEspositoS <113947910+JohnEspositoS@users.noreply.github.com>
2022-09-26 20:13:49 -0300

    ajuste de x

--- src/main/java/senac/senacfx/application/Main.java
+++ src/main/java/senac/senacfx/application/Main.java
@@ -14,6 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+
+        
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
